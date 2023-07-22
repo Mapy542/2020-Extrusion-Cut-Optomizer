5e3ba34bfdc96376fc028fd93a3c1955bc5da945
Eli <elimbukoski@gmail.com>
2023-07-22 13:39:36 -0400

    final build with docs

--- CutOptomizer.py
+++ CutOptomizer.py
@@ -42,16 +42,17 @@ for line in lines:
     except:
         Is2020.append(False)
 
+# display parsed data
 print(Lengths, Quantities, Is2020)
 
+# get user input
 maxlength = float(input("Stock Material Length: "))
 
 kerf = float(input("Saw Kerf (0 for no material lost in cuts): "))
 for i in range(len(Lengths)):
     Lengths[i] += kerf
 
-# pack 2020s
-
+# sort by type 2020/2040
 Smalls = []
 for i in range(len(Lengths)):
     if Is2020[i]:
@@ -64,15 +65,16 @@ for i in range(len(Lengths)):
         for j in range(Quantities[i]):
             Larges.append(Lengths[i])
 
+# pack 2020s
 bins = binpacking.to_constant_volume(d=Smalls, V_max=maxlength)
 for i in range(len(bins)):
     for j in range(len(bins[i])):
         bins[i][j] -= kerf
-print("2020 Packings:")
 
+# print results
+print("2020 Packings:")
 for i in range(len(bins)):
     print(bins[i])
-
 print(len(bins), " Stock Material Lengths Required")
 
 # pack 2040s
@@ -81,9 +83,20 @@ for i in range(len(bins2)):
     for j in range(len(bins2[i])):
         bins2[i][j] -= kerf
 
+# print results
 print("2040 Packings:")
-
 for i in range(len(bins2)):
     print(bins2[i])
-
 print(len(bins2), " Stock Material Lengths Required")
+
+# Save to file
+with open("2020CutsOutput.txt", "w") as f:
+    f.write("2020 Packings:\n")
+    for i in range(len(bins)):
+        f.write(str(bins[i]) + "\n")
+    f.write(str(len(bins)) + " Stock Material Lengths Required\n\n")
+    f.write("2040 Packings:\n")
+    for i in range(len(bins2)):
+        f.write(str(bins2[i]) + "\n")
+    f.write(str(len(bins2)) + " Stock Material Lengths Required\n")
+print("Saved to 2020CutsOutput.txt")
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# 2020 Extrusion Cut Optomizer
- A bin packing problem program to generate the most efficient material use from a 1D stock.
+# 2020 Extrusion Cut Optimizer
+
+A bin packing problem program to generate the most efficient material use from a 1D stock.
+
+## Getting Started
+
+The list of cut pieces are entered into the 2020Cuts.csv file in the following format:
+
+`Length, Quantity (Optional assumed 1), Type 2020/2040 (Optional assumed 2020)`
+
+The program can then be run. On the first one a required package will be installed, and then then additional information will be requested.
+
+Enter the maximum length of the stock material. The units of the stock material and the cut pieces must match but decimals are allowed.
+Then enter the thickness of the cutting blade since there will be material loss due to the blade width. This is known as the kerf of the blade.
+
+The program will then run and output the most efficient cut list to the console and to a file called 2020CutsOutput.txt. Each [] list represents one length of stock material and the numbers inside are the length of parts to cut out of that stock.
+
+As an example the material needed for my K400 Laser Cutter project is loaded. The stock includes both 2020 and 2040 material intermixed in the project. More information about the laser here: https://leboeuflasing.ddns.net/k400-laser-cutter/
